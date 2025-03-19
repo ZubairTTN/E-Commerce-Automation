70a6e36467e70ab3141c164eb486e68e16b15eee
ZubairTTN <mohd.ahmed@tothenew.com>
2025-03-19 18:39:22 +0530

    removed irrelevent comments

--- Amazon_E2E/src/main/java/com/my/app/pages/ProductPage.java
+++ Amazon_E2E/src/main/java/com/my/app/pages/ProductPage.java
@@ -8,31 +8,15 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ProductPage {
     private WebDriver driver;
-    private WebDriverWait wait;
+        private WebDriverWait wait;
 
-    private By addToCartB = By.id("a-autoid-3-announce");
-    private By productTitle = By.xpath("");
-    private By productPrice = By.xpath("");
+        private By addToCartB = By.id("a-autoid-3-announce");
 
     public ProductPage(WebDriver driver, WebDriverWait wait)
-    {
+        {
         this.driver = driver;
         this.wait = wait;
     }
-
-    public String getProductTitle()
-    {
-        WebElement titleEle = wait.until(ExpectedConditions.visibilityOfElementLocated(productTitle));
-        return titleEle.getText().trim();
-    }
-
-    public String getProductPrize()
-    {
-        WebElement priceEle = wait.until(ExpectedConditions.visibilityOfElementLocated(productPrice));
-        return priceEle.getText().trim();
-    }
-
-
     public void addToCartFunc() throws InterruptedException {
         Thread.sleep(2000);
         wait.until(ExpectedConditions.visibilityOfElementLocated(addToCartB)).click();
